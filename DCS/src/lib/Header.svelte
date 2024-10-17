3d0d6da1edb1916611953d68ab84b6cd2c82c177
Callum Walker (1000130893) <WALKCM7@student.op.ac.nz>
2024-10-18 11:04:48 +1300

    Began adding stylings to page

---
+++ DCS/src/lib/Header.svelte
@@ -0,0 +1,33 @@
+<header>
+    <h1>The GPTs Disaster Management System</h1>
+    <div class="dropdown">
+        <button class="dropbutton"></button>
+        <div class="dropdown-content">
+            <a href='#'>Civil Defence</a>
+            <a href='#'>Police</a>
+            <a href='#'>Fire and Emergency</a>
+            <a href='#'>Metservice</a>
+        </div>
+    </div>
+</header>
+
+<style>
+    * {
+        font-family: sans-serif;
+    }
+
+    header {
+        display: flex;
+        flex-wrap: nowrap;
+        justify-content: space-between;
+    }
+
+    .dropdown {
+        position: relative;
+        display: inline-block;
+    }
+
+    .dropdown-content {
+        display: block;
+    }
+</style>
--- DCS/src/routes/+page.svelte
+++ DCS/src/routes/+page.svelte
@@ -1,6 +1,12 @@
 <script>
+    import Header from '../lib/Header.svelte';
     import Footer from '../lib/Footer.svelte';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Header />
+<Footer />
+
+
+<style>
+    
+</style>
