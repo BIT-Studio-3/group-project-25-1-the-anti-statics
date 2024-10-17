821f322b435b90dac61e73b78c326d440d4ef213
George Adamson (1000124448) <ADAMGM3@student.op.ac.nz>
2024-10-18 11:57:02 +1300

    Making a start on using the volcano api from geonet

---
+++ DCS/src/routes/Geonet/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let data;
+    console.log(data);
+</script>
+
+<style>
+    div{
+        border-color: black;
+        
+    }
+</style>
+
+<h1>Volcanic Activity</h1>
+{#each data.volcanos.features as thing}
+<div>
+<h2>{thing.properties.volcanoTitle}</h2>
+<p>Activity: {thing.properties.activity}</p>
+<p>Hazard Level: {thing.properties.level}</p>
+<p>Hazards: {thing.properties.hazards}</p>
+</div>
+{/each}
