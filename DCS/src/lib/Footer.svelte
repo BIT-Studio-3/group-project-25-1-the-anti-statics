0db25a64f4b392186408712101040b3be4bf5247
Matthew39099 <VISSMJ1@student.op.ac.nz>
2024-10-18 11:30:44 +1300

    added content to the footer

--- DCS/src/lib/Footer.svelte
+++ DCS/src/lib/Footer.svelte
@@ -3,11 +3,19 @@
 </script>
 
 <footer>
-    <p>Hello, World!</p>
+    <p>Copyright © 2024 Otago Polytechnic</p>
+    <p>New Zealand 0800 762 786
+        International +64 3 477 3014
+        Hours: Monday – Friday, 8.00am – 5.00pm
+        Email: info@op.ac.nz</p>
 </footer>
 
 <style>
     footer {
         background: #689FD4;
+    }
+    p{
+        padding:10px;
+        text-align: center;
     }
 </style>
