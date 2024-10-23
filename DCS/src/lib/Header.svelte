ca3315198bfe2505221f727a75827ba64e9e68eb
Callum Walker (1000130893) <WALKCM7@student.op.ac.nz>
2024-10-23 13:41:36 +1300

    Replaced the header with something a lot better

--- DCS/src/lib/Header.svelte
+++ DCS/src/lib/Header.svelte
@@ -1,43 +1,56 @@
 <header>
     <h1>The GPTs Disaster Management System</h1>
 
-    <div class="dropdown">
-        <button class="dropdownbutton">Dropdown for other services</button>
-        <div class="dropdowncontent">
-            <a href="google.com">Police</a>
-            <a href="google.com">Fire and Emergency</a>
-            <a href="google.com">Metservice</a>
-        </div>
-    </div>
+    <nav>
+        <ul>
+            <li><a href="/weather_reports_page">Weather Reports</a></li>
+            <li><a href="/seismic_reports_page">Seismic Activity</a></li>
+            <li><a href="/road_conditions_page">Road Conditions</a></li>
+            <li><a href="/tides_page">Tidal Data</a></li>
+            <li><a href="/volcanic_acticity_page">Volcanic Activity</a></li>
+            <li><a href="/fire_and_emergency_page">Fire and Emergency Reports</a></li>
+        </ul>
+    </nav>
+
 </header>
 
 <style>
     * {
         font-family: sans-serif;
+        color: #444;
+    }
+
+    a {
+        text-decoration: none;
     }
 
     header {
         display: flex;
+        width: 100%;
+        position: relative;
+        top: 0;
         flex-wrap: nowrap;
-        justify-content: space-between;
+        justify-content: space-around;
+        align-items: center;
+        box-shadow: 0 4px 5px #333;
     }
 
-    .dropdown {
-        margin-top: 1em;
+    nav {
+        display: flex;
+        flex-direction: row;
     }
 
-    .dropdownbutton {
-        width: 15vh;
-        height: 5vh;
+    nav ul {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: nowrap;
+        flex-flow: space-around;
     }
 
-    .dropdowncontent {
-        display: none;
-        position: absolute;        
+    nav ul li {
+        list-style: none;
+        padding: 0.5em;
     }
 
-    .dropdown:hover .dropdowncontent{
-        display: block;
-    }
 
 </style>
