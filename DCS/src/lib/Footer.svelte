bb416791c8a2c9873f84c72065eeffeeb6fdb59c
Matthew39099 <VISSMJ1@student.op.ac.nz>
2024-10-23 13:38:02 +1300

    Created footer navigation to back up sites such as Metservice

--- DCS/src/lib/Footer.svelte
+++ DCS/src/lib/Footer.svelte
@@ -7,7 +7,15 @@
     <p>New Zealand 0800 762 786
         International +64 3 477 3014
         Hours: Monday – Friday, 8.00am – 5.00pm
-        Email: info@op.ac.nz</p>
+        Email: info@op.ac.nz
+    </p>
+    <nav>
+        <ul>
+            <li><a href="https://www.police.govt.nz">Police</a></li>
+            <li><a href="https://www.fireandemergency.nz">Fire and Emergency</a></li>
+            <li><a href="https://www.metservice.com/?gad_source=1&gclid=Cj0KCQjwmt24BhDPARIsAJFYKk3jeOcUrmoC2Wnv3BTx1W7KwfvELbiGuK8cJaudLvKK4y0C8V_8CaAaAiJwEALw_wcB">Metservice</a></li>
+        </ul>
+    </nav>
 </footer>
 
 <style>
@@ -18,4 +26,5 @@
         padding:10px;
         text-align: center;
     }
+    
 </style>
