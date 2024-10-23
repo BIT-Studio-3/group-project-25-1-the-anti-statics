f45a01fa8f9855f6f1cf3b79f7080559c45e71e9
WillAdamsonPolytech <adamwd1@student.op.ac.nz>
2024-10-23 23:34:57 +1300

    added fenz api

--- DCS/src/routes/IncidentResponse/+page.svelte
+++ DCS/src/routes/IncidentResponse/+page.svelte
@@ -1,3 +1,7 @@
 <script>
-    
+    async function getFENZ(){
+        const response = await fetch("https://gw.api.fireandemergency.nz/incr/v1/cadEvents/F4072896");
+        const result = await response.json;
+        console.log(result);
+    }
 </script>
