811ca0fc6e95eaae68ba0f6fbd0a49af3cc8cf4a
WillAdamsonPolytech <adamwd1@student.op.ac.nz>
2024-10-25 10:11:17 +1300

    Moved FENZ api to new page

--- DCS/src/routes/IncidentResponse/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    async function getFENZ(){
-        const response = await fetch("https://gw.api.fireandemergency.nz/incr/v1/cadEvents/F4072896");
-        const result = await response.json;
-        console.log(result);
-    }
-</script>
