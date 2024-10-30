7cd28d6821eff9b00a021d44323836148d5914d8
WillAdamsonPolytech <adamwd1@student.op.ac.nz>
2024-10-30 14:37:53 +1300

    I think the RSS is working

--- DCS/src/routes/rss/+page.svelte
+++ DCS/src/routes/rss/+page.svelte
@@ -2,10 +2,22 @@
   import Header from "$lib/Header.svelte";
   import Footer from "$lib/Footer.svelte";
   import { onMount } from "svelte";
-  export let data = {};
+  
+  onMount(() => {
+    console.log(window); // Now safe to access 'window' since this runs only in the browser
+  });
+  
+  const RSS_URL = `https://cors-anywhere.herokuapp.com/https://alerthub.civildefence.govt.nz/rss/pwp`;
+  fetch(RSS_URL)
+    .then((response) => response.text())
+    .then((str) => new window.DOMParser().parseFromString(str, "text/xml"))
+    .then((url1) => console.log(url1));
 
-  console.log(data);
-
+    const RSS_URL2=`https://cors-anywhere.herokuapp.com/https://alerts.metservice.com/cap/rss`
+    fetch(RSS_URL2)
+    .then((response) => response.text())
+    .then((str) => new window.DOMParser().parseFromString(str, "text/xml"))
+    .then((url2) => console.log(url2));
 
 </script>
 
