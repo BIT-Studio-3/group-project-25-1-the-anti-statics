e39ba5c5724f2a2057f0ffedf84b951a18c6fb4f
George Adamson (1000124448) <ADAMGM3@student.op.ac.nz>
2024-10-30 14:50:08 +1300

    Basically nothing

---
+++ DCS/src/routes/Alerts/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Header from '$lib/Header.svelte';
+    import Footer from '$lib/Footer.svelte';
+</script>
+
+<Header />
+
+<h1>Alerts</h1>
+<h2>Post an alert</h2>
+<p>This where users will be able to post alerts. They will be able to Choose alert type, level, and region.</p>
+
+<Footer />
