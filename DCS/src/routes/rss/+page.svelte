1e4a0659ec057838f4bbbbadeec38ad8b9a0ef43
WillAdamsonPolytech <adamwd1@student.op.ac.nz>
2024-10-30 13:35:23 +1300

    Mocked up RSS feed card as there are no actual alerts

--- DCS/src/routes/rss/+page.svelte
+++ DCS/src/routes/rss/+page.svelte
@@ -43,8 +43,8 @@
 
 <Header />
 
-<main>
-  <h1>Combined Alerts</h1>
+
+  <h1>Alert Feed</h1>
   <ul>
     {#each mergedAlerts as alert}
       <li>
@@ -53,6 +53,30 @@
       </li>
     {/each}
   </ul>
-</main>
+
+  <div class="card">
+    <h3>Red Heavy Rain Warning</h3>
+    <p>Dunedin, N. Otago and coastal Clutha</p>
+    <p>Until 9pm Friday</p>
+    <p>Expect 70-100mm of rain on top of what has already fallen. Heaviest rain around eastern hills</p>
+  </div>
 
 <Footer />
+
+<style>
+h1{
+  font-size: 2em;
+}
+h3{
+  background-color:orangered;
+  padding: 4px;
+  border-radius: 5px;
+}
+.card{
+padding: 10px;
+margin: 10px;
+border:2px solid black;
+border-radius: 5px;
+width: 10em;
+}
+</style>
