ff24879fd06aa891609580ee4ef9e8128b7aa71f
WillAdamsonPolytech <adamwd1@student.op.ac.nz>
2024-11-01 11:28:56 +1300

    We actually have a working RSS now

--- DCS/src/routes/rss/+page.svelte
+++ DCS/src/routes/rss/+page.svelte
@@ -2,27 +2,11 @@
   import Header from "$lib/Header.svelte";
   import Footer from "$lib/Footer.svelte";
   import { onMount } from "svelte";
-
-  onMount(() => {
-    console.log(window); //Now safe to use new window twice, would crash before.
-  });
-
-  const RSS_URL = `https://cors-anywhere.herokuapp.com/https://alerthub.civildefence.govt.nz/rss/pwp`;
-  fetch(RSS_URL)
-    .then((response) => response.text())
-    .then((str) => new window.DOMParser().parseFromString(str, "text/xml"))
-    .then((url1) => console.log(url1));
-
-  const RSS_URL2 = `https://cors-anywhere.herokuapp.com/https://alerts.metservice.com/cap/rss`;
-  fetch(RSS_URL2)
-    .then((response) => response.text())
-    .then((str) => new window.DOMParser().parseFromString(str, "text/xml"))
-    .then((url2) => console.log(url2));
 </script>
 
 <Header />
 
-<div class="card">
+<!-- <div class="card">
   <h3>Red Heavy Rain Warning</h3>
   <p>Dunedin, N. Otago and coastal Clutha</p>
   <p>Until 9pm Friday</p>
@@ -30,6 +14,15 @@
     Expect 70-100mm of rain on top of what has already fallen. Heaviest rain
     around eastern hills
   </p>
+</div> -->
+
+<div>
+  <iframe
+    title="rss"
+    class="rss"
+    src="https://rss.app/embed/v1/wall/G4sXEJ0UJN3Owjow"
+    frameborder="0"
+  ></iframe>
 </div>
 
 <Footer />
@@ -49,5 +42,9 @@
     border: 2px solid black;
     border-radius: 5px;
     width: 10em;
+  }
+  .rss {
+    width: 50%;
+    height: 29em;
   }
 </style>
