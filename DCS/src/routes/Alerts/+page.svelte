98323b7678d7677ded9837adab8c4a48e5feef4a
George Adamson (1000124448) <ADAMGM3@student.op.ac.nz>
2024-11-01 11:04:00 +1300

    Concept for an alerts page

--- DCS/src/routes/Alerts/+page.svelte
+++ DCS/src/routes/Alerts/+page.svelte
@@ -3,10 +3,71 @@
     import Footer from '$lib/Footer.svelte';
 </script>
 
+<style>
+    * {
+        font-family: sans-serif;
+        color: #333
+    }
+    textarea{
+        vertical-align: top;
+        height: 10em;
+        width: 25em;
+    }
+    form{
+        border-color: black;
+        border: solid;
+        padding: 1em;
+    }
+</style>
+
 <Header />
 
 <h1>Alerts</h1>
 <h2>Post an alert</h2>
-<p>This where users will be able to post alerts. They will be able to Choose alert type, level, and region.</p>
+<p>This where users will be able to post alerts. They will be able to choose emergency type, level, and region.</p>
+<p>Alerts will show up in a feed on another page. This form currently does nothing. Once we learn more about logins, this page will be functional.</p>
+
+<form>
+    <h2>Post Alert:</h2>
+    <p>
+        <label for="title">Title:</label>
+        <input typ="text" id="title" name="alert-title"/>
+    </p>
+    <p>
+        <label for="type">Emergency type:</label>
+        <select for="type" id="type">
+            <option value="">Please select an emergency type</option>
+            <option value="fire">Fire</option>
+            <option value="flood">Flooding</option>
+            <option value="quake">Earthquake damage</option>
+        </select>
+    </p>
+    <p>
+        <label for="level">Select alert level:</label>
+        <select name="level" id="level">
+            <option value="one">1</option>
+            <option value="two">2</option>
+            <option value="three">3</option>
+            <option value="four">4</option>
+            <option value="five">5</option>
+        </select>
+    </p>
+    <p>
+        <label for="region">Choose Region:</label>
+        <select name="region" id="region">
+            <option value="">Please select a region</option>
+            <option value="otago">Otago</option>
+            <option value="canterbury">Canterbury</option>
+            <option value="southland">Southland</option>
+        </select>
+    </p>
+    <p>
+        <label for="description">Description:</label>
+        <textarea id="description" name="description"></textarea>
+    </p>
+    <p >
+        <button type="submit">Submit</button>
+    </p>
+</form>
 
 <Footer />
