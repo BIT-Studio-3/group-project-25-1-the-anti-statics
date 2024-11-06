7749993bffca706094c50e305661d97059a10e02
Matthew39099 <VISSMJ1@student.op.ac.nz>
2024-11-06 14:17:58 +1300

    Added colour code with heading for the hazard levels Might be better UX to move the code to the bottom but less functional need to discuss with team

--- DCS/src/routes/volcanic_activity_page/+page.svelte
+++ DCS/src/routes/volcanic_activity_page/+page.svelte
@@ -1,4 +1,4 @@
-<script>    
+<script>
     export let data;
     const volcanos = data.volcanos;
     console.log(volcanos);
@@ -7,45 +7,53 @@
 
     function displayVolcanoCards() {
         if (volcanos && volcanos.features) {
-            volcanoCards = volcanos.features.map(volcano => ({
+            volcanoCards = volcanos.features.map((volcano) => ({
                 title: volcano.properties.volcanoTitle,
                 activity: volcano.properties.activity,
                 hazardLevel: volcano.properties.level,
                 hazards: volcano.properties.hazards,
-                id: volcano.properties.volcanoTitle.replace(/\s+/g, '-') // Create ID
+                id: volcano.properties.volcanoTitle.replace(/\s+/g, "-"), // Create ID
             }));
 
             // Sort alphabetically by ID
             volcanoCards.sort((a, b) => a.id.localeCompare(b.id));
-
         }
     }
 
     function getCardBackground(card) {
-        switch(card.hazardLevel) {
+        switch (card.hazardLevel) {
             case 1:
-                return '#3D995D';
+                return "#3D995D";
             case 2:
-                return '#0000FF';
+                return "#0000FF";
             case 3:
-                return '#FFFF00';
+                return "#FFFF00";
             case 4:
-                return '#FF6600';
+                return "#FF6600";
             case 5:
-                return '#FE0000';
+                return "#FE0000";
         }
     }
 
     displayVolcanoCards();
-
-    
 </script>
 
 <h2>Volcanic Activity</h2>
-
+<h3>Colour Code:</h3>
+<div class="Code">
+    <div class="one">1 █</div>
+    <div class="two">2 █</div>
+    <div class="three">3 █</div>
+    <div class="four">4 █</div>
+    <div class="five">5 █</div>
+</div>
 <div class="cardcontainer">
     {#each volcanoCards as card}
-        <div class="volcanocard" id={card.id} style="background-color: {getCardBackground(card)};">
+        <div
+            class="volcanocard"
+            id={card.id}
+            style="background-color: {getCardBackground(card)};"
+        >
             <h3>{card.title}</h3>
             <p>Activity: {card.activity}</p>
             <p>Hazard Level: {card.hazardLevel}</p>
@@ -55,7 +63,6 @@
 </div>
 
 <style>
-
     h2 {
         margin-top: 0.5em;
         margin-left: 1em;
@@ -87,5 +94,34 @@
         padding: 0.8em;
         background: inherit;
         border-radius: 5px;
+    }
+    .Code {
+        padding: 0.8em;
+        margin-left: 1em;
+    }
+    h3{
+        font-size: large;
+        margin-left: 1em;
+    }
+
+    .one {
+        color: #3d995d;
+        padding: 0.2em;
+    }
+    .two {
+        color: #0000ff;
+        padding: 0.2em;
+    }
+    .three {
+        color: #ffff00;
+        padding: 0.2em;
+    }
+    .four {
+        color: #ff6600;
+        padding: 0.2em;
+    }
+    .five {
+        color: #fe0000;
+        padding: 0.2em;
     }
 </style>
