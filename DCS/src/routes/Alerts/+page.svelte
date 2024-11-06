51d4287ec06acea1adfe7bc2fabb412f9f1df925
George Adamson (1000124448) <ADAMGM3@student.op.ac.nz>
2024-11-06 14:45:17 +1300

    Draining the div soup

--- DCS/src/routes/Alerts/+page.svelte
+++ DCS/src/routes/Alerts/+page.svelte
@@ -17,48 +17,45 @@
 
 <form>
   <h2>Post Alert:</h2>
-  <div>
-    <div class="container">
-      <label for="title">Title:</label>
-      <div>
-        <input typ="text" id="title" name="alert-title" />
-      </div>
-      <label for="type">Emergency type:</label>
-      <div>
-      <select for="type" id="type">
-        <option value="">Please select an emergency type</option>
-        <option value="fire">Fire</option>
-        <option value="flood">Flooding</option>
-        <option value="quake">Earthquake damage</option>
-      </select>
-    </div>
+  <div class="container">
+    <label for="title">Title:</label>
+
+    <input typ="text" id="title" name="alert-title" />
+
+    <label for="type">Emergency type:</label>
+
+    <select for="type" id="type">
+      <option value="">Please select an emergency type</option>
+      <option value="fire">Fire</option>
+      <option value="flood">Flooding</option>
+      <option value="quake">Earthquake damage</option>
+    </select>
+
     <label for="level">Select alert level:</label>
-      <div>
-        <select name="level" id="level">
-          <option value="one">1</option>
-          <option value="two">2</option>
-          <option value="three">3</option>
-          <option value="four">4</option>
-          <option value="five">5</option>
-        </select>
-      </div>
-      <label for="region">Choose Region:</label>
-      <div>
-        <select name="region" id="region">
-          <option value="">Please select a region</option>
-          <option value="otago">Otago</option>
-          <option value="canterbury">Canterbury</option>
-          <option value="southland">Southland</option>
-        </select>
-      </div>
-      <label for="description">Description:</label>
-      <div>
-        <textarea id="description" name="description"></textarea>
-      </div>
-    </div>
-    <div>
-      <button type="submit">Submit</button>
-    </div>
+
+    <select name="level" id="level">
+      <option value="one">1</option>
+      <option value="two">2</option>
+      <option value="three">3</option>
+      <option value="four">4</option>
+      <option value="five">5</option>
+    </select>
+
+    <label for="region">Choose Region:</label>
+
+    <select name="region" id="region">
+      <option value="">Please select a region</option>
+      <option value="otago">Otago</option>
+      <option value="canterbury">Canterbury</option>
+      <option value="southland">Southland</option>
+    </select>
+
+    <label for="description">Description:</label>
+
+    <textarea id="description" name="description"></textarea>
+  </div>
+  <div>
+    <button type="submit">Submit</button>
   </div>
 </form>
 
@@ -78,11 +75,11 @@
     border-color: black;
     border: solid;
     padding: 1em;
-    align-items: start;
   }
   label {
     text-align: right;
-    padding-top: 8px;
+    justify-content: center;
+    padding-top: 10px;
   }
   div {
     padding: 0.5em;
@@ -91,11 +88,22 @@
     display: grid;
     grid-template-columns: 10% 90%;
   }
-  @media (max-width: 1281px){ 
-    .container{
-      display:block
+  input {
+    max-width: fit-content;
+    margin: 10px;
+  }
+  select{
+    max-width: fit-content;
+    margin: 10px;
+  }
+  textarea{
+    margin: 10px;
+  }
+  @media (max-width: 1281px) {
+    .container {
+      display: block;
     }
-    textarea{
+    textarea {
       width: auto;
     }
   }
