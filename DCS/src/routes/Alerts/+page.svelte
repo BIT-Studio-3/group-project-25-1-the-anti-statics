1093d615dfc09e44fc06ef2502a86160e2256b3c
Adamgm3 <ADAMGM3@student.op.ac.nz>
2024-11-12 15:15:12 +1300

    Removing extra header and footer

--- DCS/src/routes/Alerts/+page.svelte
+++ DCS/src/routes/Alerts/+page.svelte
@@ -1,10 +1,3 @@
-<script>
-  import Header from "$lib/Header.svelte";
-  import Footer from "$lib/Footer.svelte";
-</script>
-
-<Header />
-
 <h1>Alerts</h1>
 <p>
   This where users will be able to post alerts. They will be able to choose
@@ -58,8 +51,6 @@
     <button type="submit">Submit</button>
   </div>
 </form>
-
-<Footer />
 
 <style>
   * {
