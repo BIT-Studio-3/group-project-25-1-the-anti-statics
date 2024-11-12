b48235077e1eeaace3901267bc0485caffb4eb13
WillAdamsonPolytech <adamwd1@student.op.ac.nz>
2024-11-13 10:14:54 +1300

    Revert "Merge branch 'main' into StylingImprovements"
    This reverts commit 648d0057c5aaf7e60ae9d85f493b33e0e61e7846, reversing
    changes made to 536a549e292f077dc250aabc7eb21b6377cb5fd7.
    Fixing my mess up.

--- DCS/src/lib/Header.svelte
+++ DCS/src/lib/Header.svelte
@@ -9,14 +9,13 @@
 
     <nav>
         <ul>
+            <a href="/weather_reports_page" class="firstrow"><li class:active={route === "/weather_reports_page"}>Weather Reports</li></a>
+            <a href="/seismic_reports_page" class="firstrow"><li class:active={route === "/seismic_reports_page"}>Seismic Activity</li></a>
+            <a href="/road_conditions_page" class="firstrow"><li class:active={route === "/road_conditions_page"}>Road Conditions</li></a>
+            <a href="/volcanic_activity_page" class="secondrow"><li class:active={route === "/volcanic_activity_page"}>Volcanic Activity</li></a>
+            <a href="/fire_and_emergency_page" class="secondrow"><li class:active={route === "/fire_and_emergency_page"}>Fire and Emergency Reports</li></a>
+            <a href="/rss" class="secondrow"><li class:active={route === "/rss"}>RSS Feed</li></a>
 
-            <a href="/weather_reports_page"><li class:active={route === "/weather_reports_page"}>Weather Reports</li></a>
-            <a href="/seismic_reports_page"><li class:active={route === "/seismic_reports_page"}>Seismic Activity</li></a>
-            <a href="/road_conditions_page"><li class:active={route === "/road_conditions_page"}>Road Conditions</li></a>
-            <a href="/volcanic_activity_page"><li class:active={route === "/volcanic_activity_page"}>Volcanic Activity</li></a>
-            <a href="/fire_and_emergency_page"><li class:active={route === "/fire_and_emergency_page"}>Fire and Emergency Reports</li></a>
-            <a href="/rss"><li class:active={route === "/rss"}>RSS Feed</li></a>
-            <a href="/Alerts"><li class:active={route === "/Alerts"}>Post an Alert</li></a>
         </ul>
     </nav>
 
--- DCS/src/routes/rss/+page.svelte
+++ DCS/src/routes/rss/+page.svelte
@@ -6,13 +6,30 @@
   <iframe
     title="rss"
     class="rss"
-    src="https://rss.app/embed/v1/wall/LgTbpx1cNjP0QQF4"
+    src="https://rss.app/embed/v1/wall/G4sXEJ0UJN3Owjow"
     frameborder="0"
   ></iframe>
 
 </div>
 
 <style>
+  h1 {
+    font-size: 2em;
+  }
+  h3 {
+    background-color: orangered;
+    color: black;
+    padding: 4px;
+    border-radius: 5px;
+    margin-bottom: 5px;
+  }
+  .card {
+    padding: 10px;
+    margin: 10px;
+    border: 2px solid black;
+    border-radius: 5px;
+    width: 10em;
+  }
   .rss {
     width: 100%;
     height: 100vh;
