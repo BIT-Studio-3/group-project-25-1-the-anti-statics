774519013d60572970067b08f0481874333d52d7
Samuel Batchelor <batcsg1@student.op.ac.nz>
2025-03-03 15:37:48 +1300

    feat: added agency select file

---
+++ src/lib/agencySelect.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { selectedAgency } from "$lib/stores.js";
+</script>
+
+<select bind:value={$selectedAgency}>
+    <option value="All">All</option>
+    <option value="FENZ">FENZ</option>
+    <option value="GeoNet">GeoNet</option>
+    <option value="USAR">USAR</option>
+</select>
+
+<style>
+</style>
