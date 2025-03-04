804c4495bff290cca202221ba1abadf058185094
SamBat2005 <batcsg1@student.op.ac.nz>
2025-03-04 20:06:52 +1300

    feat(javascript): added if block for agency specific view on weather reports page

--- src/routes/weather_reports_page/+page.svelte
+++ src/routes/weather_reports_page/+page.svelte
@@ -1,15 +1,22 @@
 <script>
-
+    import { selectedAgency } from '$lib/stores.js';
 </script>
 
+
+{#if $selectedAgency === "MetService"}
 <h1>Metservice Stuff</h1>
-
+{:else if $selectedAgency === "NEMA"}
 <h1>NEMA Stuff</h1>
-
+{:else if $selectedAgency === "FENZ"}
 <h1>FENZ Stuff</h1>
-
+{:else if $selectedAgency === "Waka Kotahi"}
 <h1>Waka Kotahi Stuff</h1>
-
+{:else}
+<h1>Metservice Stuff</h1>
+<h1>NEMA Stuff</h1>
+<h1>FENZ Stuff</h1>
+<h1>Waka Kotahi Stuff</h1>
+{/if}
 
 <style>
 
