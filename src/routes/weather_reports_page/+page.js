5bb97f173301d0b9acd94e707a956aae1c2bedd5
SamBat2005 <batcsg1@student.op.ac.nz>
2025-03-04 19:45:43 +1300

    refactor(javascript): commented out API function for weather reports

--- src/routes/weather_reports_page/+page.js
+++ src/routes/weather_reports_page/+page.js
@@ -1,6 +1,6 @@
-export async function load ({fetch}) {
-    const response = await fetch('');
-    const weather = await response.json();
-    return {weather};
-}
+// export async function load ({fetch}) {
+//     const response = await fetch('');
+//     const weather = await response.json();
+//     return {weather};
+// }
 // USE TOMTOM FOR API'S, SWITCH CODE AROUND TO FIT TRAFFIC API, CREATE PAGE FOR TRAFFIC DATA, INCLUDING DROP DOWN MENU/ACCORDIANS
