43d9b30aef2cbdd6c067904d92811f90189c5fc5
Samuel Batchelor <batcsg1@student.op.ac.nz>
2025-03-10 16:25:38 +1300

    refactor: added Metservice rss frame to weather reports page

--- src/routes/weather_reports_page/+page.svelte
+++ src/routes/weather_reports_page/+page.svelte
@@ -12,7 +12,12 @@
 {:else if $selectedAgency === "Waka Kotahi"}
 <h1>Waka Kotahi Stuff</h1>
 {:else}
-<h1>Metservice Stuff</h1>
+<iframe
+    title="rss"
+    class="rss"
+    src="https://rss.app/embed/v1/wall/LgTbpx1cNjP0QQF4"
+    frameborder="0"
+></iframe>
 <h1>NEMA Stuff</h1>
 <h1>FENZ Stuff</h1>
 <h1>Waka Kotahi Stuff</h1>
