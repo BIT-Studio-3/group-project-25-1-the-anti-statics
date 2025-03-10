255373303063e3dad6f820a7b38cce0143cb6ad2
batcsg1 <batcsg1@student.op.ac.nz>
2025-03-10 21:37:05 +1300

    refactor(js): removed volcanic activity agency map

--- src/lib/agencySelect.svelte
+++ src/lib/agencySelect.svelte
@@ -7,7 +7,6 @@
         "/weather_reports_page": ["MetService", "NEMA"],
         "/seismic_reports_page": ["GeoNet", "NEMA", "NZDF"],
         "/road_conditions_page": ["Waka Kotahi", "KiwiRail", "NZ Police", "Local Councils"],
-        "/volcanic_activity_page": ["GeoNet", "NEMA", "Airways NZ", "CDEM"],
         "/fire_and_emergency_page": ["FENZ", "NZ Police", "DOC", "NEMA"]
     };
 
