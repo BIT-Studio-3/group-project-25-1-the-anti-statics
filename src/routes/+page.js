f11619634d6be2adcdff8acfb73ec51e1bff424c
batcsg1 <batcsg1@student.op.ac.nz>
2025-03-14 12:38:53 +1300

    feat(js): added code comments in main page.js file

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,20 +1,25 @@
 export async function load() {
     try {
+        //Fetch the data from the back-end API
         const response = await fetch('http://localhost:3000/api/v1/alerts');
-
+        
+        //If the response has no data
         if(!response.ok){
             return {message: "No alerts available"};
         }
-
+        
+        //Fetch the data in json format
         const alerts = await response.json();
-
+        
+        //Console the data
         console.log(alerts);
         
+        //Return the data
         return {alerts};
     } catch (error) {
-
+        //Console the error
         console.log('Error fetching data:', error);
-
+        //Return the error if the API is offline
         return {error: 'The server is currently unreachable. Please try again later.'}
     }
 }
