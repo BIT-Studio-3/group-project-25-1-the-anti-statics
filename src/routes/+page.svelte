2ca231a9fe8a98d4b44521777e9f24f4627073bb
batcsg1 <batcsg1@student.op.ac.nz>
2025-03-14 10:53:38 +1300

    refactor(html): formatted code with prettier and added if block for alerts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,82 +1,83 @@
 <script>
+  import { selectedAgency } from "$lib/stores.js";
 
-    import { selectedAgency } from '$lib/stores.js';
+  import Map from "../lib/Map.svelte";
 
-
-
-
-    import Map from '../lib/Map.svelte';
-
-    //Export the load function
-    export let data;
-    const {alerts, error} = data;
+  //Export the load function
+  export let data;
+  const { alerts, error } = data;
 </script>
 
 <div class="PageContentContainer">
+  {#if error}
+    <div>{error}</div>
+  {:else}
     <div class="AlertsCard">
-        <h3>Recent Alerts</h3>
-        <ul class="AlertsList">
-            <li>A cave has collapsed in Southland<br>
-                1 injury, 1 unknown<br>
-            <em>1/11/2024 10:20am</em></li>
-            <li>Theres been a mutiny in Nelson<br>
-                <em>18/10/2024 9:32am</em></li>
-        </ul>
+      <h3>Recent Alerts</h3>
+      <ul class="AlertsList">
+        <li>
+          A cave has collapsed in Southland<br />
+          1 injury, 1 unknown<br />
+          <em>1/11/2024 10:20am</em>
+        </li>
+        <li>
+          Theres been a mutiny in Nelson<br />
+          <em>18/10/2024 9:32am</em>
+        </li>
+      </ul>
     </div>
+  {/if}
 
-    <figure class="pinmap">
-        <h3>Active Disasters</h3>
-        <Map />
-    </figure>
-    
+  <figure class="pinmap">
+    <h3>Active Disasters</h3>
+    <Map />
+  </figure>
 </div>
 
 <style>
+  h3 {
+    margin-top: 0.5em;
+    font-size: 180%;
+    text-align: center;
+  }
 
-    h3 {
-        margin-top: 0.5em;
-        font-size: 180%;
-        text-align: center;
-    }
+  img {
+    max-height: 90%;
+    width: auto;
+  }
 
-    img {
-        max-height: 90%;
-        width: auto;
-    }
+  .PageContentContainer {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    height: 100%;
+    width: 100%;
+  }
 
-    .PageContentContainer {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        height: 100%;
-        width: 100%;
-    }
+  .AlertsCard {
+    display: flex;
+    flex-direction: column;
+    border: solid;
+    width: 25em;
+    min-height: 80vh;
+    justify-self: left;
+  }
 
-    .AlertsCard{
-        display: flex;
-        flex-direction: column;
-        border: solid;
-        width: 25em;
-        min-height: 80vh;
-        justify-self: left;
-    }
+  .AlertsList li {
+    margin-top: 0.5em;
+    list-style: none;
+    padding-left: 0.5em;
+  }
 
-    .AlertsList li {
-        margin-top: 0.5em;
-        list-style: none;
-        padding-left: 0.5em;
-    }
+  .pinmap {
+    max-height: 100%;
+    width: 600px;
+    border: solid;
+    text-align: center;
+    background-color: #b7daf8;
+  }
 
-    .pinmap {
-        max-height: 100%;
-        width: 600px;
-        border: solid;
-        text-align: center;
-        background-color: #B7DAF8;
-    }
-
-    .pinmap h3 {
-        background-color: #B7DAF8;
-    }
-
+  .pinmap h3 {
+    background-color: #b7daf8;
+  }
 </style>
