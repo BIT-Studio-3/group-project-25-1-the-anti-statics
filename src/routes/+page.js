0b91685226721ee227dd9996aea9e7b48d05f46f
batcsg1 <batcsg1@student.op.ac.nz>
2025-03-14 10:47:01 +1300

    feat(js): added code for try-catch in load function

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,8 +1,18 @@
 export async function load() {
     try {
         const response = await fetch('http://localhost:3000/api/v1/alerts');
-        
+
+        if(!response.ok){
+            throw new Error('Failed to fetch data');
+        }
+
+        const alerts = await response.json();
+
+        return {alerts};
     } catch (error) {
         
+        console.log('Error fetching data:', error);
+
+        return {error: 'The server is currently unreachable. Please try again later.'}
     }
 }
