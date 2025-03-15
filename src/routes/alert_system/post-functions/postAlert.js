da670a629d0891685990772e045c778c1adeec33
batcsg1 <batcsg1@student.op.ac.nz>
2025-03-15 22:33:29 +1300

    refactor(js): added error handling for alert level field

--- src/routes/alert_system/post-functions/postAlert.js
+++ src/routes/alert_system/post-functions/postAlert.js
@@ -1,26 +1,33 @@
 export async function postAlert(alertData) {
-    try {
-      console.log('Posting alert with data:', alertData);
-      
-      const response = await fetch('http://localhost:3000/api/v1/alerts', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(alertData)
-      });
-  
-      if (!response.ok) {
-        return {postError: 'Failed to post alert. Please try again later.'}
-      }
-  
-      const data = await response.json();
-      return {data}; // Return the response data (e.g., the created alert)
-    } catch (error) {
-      //Console the error
-      console.log(error);
-      //Return the error if the API is offline
-      return {error: 'The server is currently unreachable. Please try again later.'}
+  try {
+    console.log("Posting alert with data:", alertData);
+
+    // Check if alertLevel is valid
+    if (isNaN(alertData.alertLevel)) {
+      return { postError: "Invalid alert level. It must be a number." };
     }
+
+    alertData.alertLevel = parseInt(alertData.alertLevel);
+    const response = await fetch("http://localhost:3000/api/v1/alerts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(alertData),
+    });
+
+    if (!response.ok) {
+      return { postError: "Failed to post alert. Please try again later." };
+    }
+
+    const data = await response.json();
+    return { data }; // Return the response data (e.g., the created alert)
+  } catch (error) {
+    //Console the error
+    console.log(error);
+    //Return the error if the API is offline
+    return {
+      error: "The server is currently unreachable. Please try again later.",
+    };
   }
-  
+}
