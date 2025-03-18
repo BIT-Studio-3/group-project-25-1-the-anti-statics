a574846572a07d147ff959415fa10760398c1a88
SamBat2005 <batcsg1@student.op.ac.nz>
2025-03-18 18:12:36 +1300

    feat: created camera component

---
+++ src/lib/Cameras.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let data;
+  const { cameras, error, message } = data;
+
+  console.log(cameras);
+</script>
+
+<section>
+  {#each cameras as camera}
+    <li>{camera.id}</li>
+  {/each}
+</section>
