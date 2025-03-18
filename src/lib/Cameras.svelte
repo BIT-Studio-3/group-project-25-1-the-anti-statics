a3cdac436ca37ce0a2fd11989b8d54423228f5f6
SamBat2005 <batcsg1@student.op.ac.nz>
2025-03-18 18:18:29 +1300

    feat: added camera selection feature to road conditions page

--- src/lib/Cameras.svelte
+++ src/lib/Cameras.svelte
@@ -3,10 +3,32 @@
   const { cameras, error, message } = data;
 
   console.log(cameras);
+  let selectedCameraId = null;
+  let selectedCamera = null;
+
+  // Find the selected camera based on the ID
+  $: selectedCamera = cameras.find(
+    (camera) => camera.id === selectedCameraId
+  );
 </script>
 
 <section>
-  {#each cameras as camera}
-    <li>{camera.id}</li>
-  {/each}
+  <h2 class="text-2xl font-bold mb-2">NZTA Traffic Camera</h2>
+
+  <select bind:value={selectedCameraId}>
+    <option value="" disabled>Select a camera</option>
+    {#each cameras as camera}
+      <option value={camera.id}>{camera.name}</option>
+    {/each}
+  </select>
+
+  {#if selectedCamera}
+    <div>
+      <img
+        src={selectedCamera.imageUrl}
+        alt={selectedCamera.name || "Image Loading"}
+      />
+      <p>{selectedCamera.description}</p>
+    </div>
+  {/if}
 </section>
