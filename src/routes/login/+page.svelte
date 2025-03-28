dc6bcc981b74c47eae027547365fbdab5ee7f283
SamBat2005 <batcsg1@student.op.ac.nz>
2025-03-28 17:10:47 +1300

    feat(html): added login form

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,8 +1,42 @@
 <script>
+  import login from "./utils/postLogins.js";
+  let username = "";
+  let password = "";
+  let error = "";
 
+  const submitLogin = async (event) => {
+    // Form data object
+    const loginData = {
+      username,
+      password,
+    };
+
+    const result = await login(loginData);
+
+    data = result.data;
+    error = result.error;
+
+    console.log("Post data:", data);
+    console.log("Error:", error);
+
+    if (result.data) {
+      alert("Login posted successfully!");
+    }
+  };
 </script>
+
 <h1>Login</h1>
+<form on:submit|preventDefault={submitLogin}>
+  <div>
+    <label for="username">Username</label>
+    <input type="text" id="username" bind:value={username} />
+  </div>
+  <div>
+    <label for="password">Password</label>
+    <input type="password" id="password" bind:value={password} />
+  </div>
+  <button type="submit">Submit</button>
+</form>
 
 <style>
-
 </style>
