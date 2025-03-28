5e7509dfaba9891b867ea248ff9dc899a3029732
SamBat2005 <batcsg1@student.op.ac.nz>
2025-03-28 15:35:36 +1300

    feat(svelte): added feature where default layout is hidden on login page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,23 @@
 <script>
+    import { page } from "$app/stores";
     import '../app.css';
     import Header from '../lib/Header.svelte';
     import Footer from '../lib/Footer.svelte';
+
+    let login = false;
+
+  // Watch for route changes
+  $: {
+    login = $page.url.pathname.startsWith("/login");
+  }
 </script>
 
-<Header />
+{#if !login}
+  <Header />
+{/if}
 
 <slot />
 
-<Footer />
+{#if !login}
+  <Footer />
+{/if}
