79d62b1c0aa592ad118ba22492e3a76986d21c45
SamBat2005 <batcsg1@student.op.ac.nz>
2025-03-28 22:14:23 +1300

    feat: added more flash styling to login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -27,21 +27,42 @@
 
   import user from "../../stores/user.js";
   $: login = $user === null ? false : true;
+
+  import logo from "../../lib/Images/dma.png";
 </script>
 
 {#if !login}
-  <h1>Login</h1>
-  <form on:submit|preventDefault={submitLogin}>
-    <div>
-      <label for="username">Username</label>
-      <input type="text" id="username" bind:value={username} />
+  <main>
+    <div id="form-container">
+      <div id="image-container">
+        <img src={logo} alt="logo" width=200 height=110>
+      </div>
+      <div id="login-header">
+        <h1>Sign in</h1>
+      </div>
+      <form on:submit|preventDefault={submitLogin}>
+        <div id="username">
+          <input
+            type="text"
+            id="username"
+            placeholder="Username"
+            bind:value={username}
+          />
+        </div>
+        <div id="password">
+          <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            bind:value={password}
+          />
+        </div>
+        <div id="submit">
+          <button type="submit">Sign in</button>
+        </div>
+      </form>
     </div>
-    <div>
-      <label for="password">Password</label>
-      <input type="password" id="password" bind:value={password} />
-    </div>
-    <button type="submit">Submit</button>
-  </form>
+  </main>
 
   {#if error}
     <p style="color: red;">{error}</p>
@@ -53,4 +74,43 @@
 {/if}
 
 <style>
+  main {
+    display: grid;
+    place-items: center;
+    height: 30em;
+  }
+  #form-container {
+    padding: 1em;
+    background-color: white;
+    border: 2px dashed rgb(125, 216, 160);
+    border-radius: 3%;
+    width: 20em;
+    box-shadow: 10px 10px 10px rgb(182, 173, 173);
+  }
+  #form-container div {
+    display: flex;
+    flex-direction: column;
+    padding: 0.5em;
+    background-color: white;
+  }
+  #login-header {
+    text-align: center;
+  }
+  form {
+    background-color: white;
+  }
+  #form-container div * {
+    background-color: white;
+  }
+  #submit button {
+    background-color: #b5d5c5;
+  }
+  input,
+  button {
+    padding: 0.3em;
+    box-shadow: 3px 3px 3px #333;
+  }
+  #image-container{
+    align-items: center;
+  }
 </style>
