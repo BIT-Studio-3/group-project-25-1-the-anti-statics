f59f7bf971c56517b04772af11cdd7ffca3e096a
SamBat2005 <batcsg1@student.op.ac.nz>
2025-03-28 18:01:59 +1300

    feat: added more conditional handling to ensure user doesn't see website when not logged in

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,18 +6,28 @@
   import user from "../stores/user.js";
   import { goto } from "$app/navigation";
   import { onMount } from "svelte";
+  import { page } from "$app/stores";
+
+  let login = false;
+  let isLoginPage = false;
+  let loading = true; // Add a loading state
 
   $: login = $user === null ? false : true;
+  $: isLoginPage = $page.url.pathname === "/login";
 
   onMount(() => {
-    !login ? goto("/login") : goto("/");
+    !login && !isLoginPage ? goto("/login") : loading = false;
   });
 </script>
 
-{#if !login}
-  <slot/>
-{:else}
+{#if loading}
+  <p>Loading...</p> <!-- Show loading message until the state is settled -->
+{:else if login}
   <Header />
   <slot />
   <Footer />
+{:else if isLoginPage}
+  <slot/> <!-- Content for the login page will be shown here -->
+{:else}
+  <!-- Nothing is shown if the user is not logged in and we're not on the login page -->
 {/if}
