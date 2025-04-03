61b1f14cfac1a4b0ca6913463519954d36da6903
Hazel <mapuhn1@student.op.ac.nz>
2025-04-04 09:29:03 +1300

    fix : update data url to render url

--- src/routes/damage_record_page/post-functions/postDamage.js
+++ src/routes/damage_record_page/post-functions/postDamage.js
@@ -1,31 +1,29 @@
 export async function postDamage(damageData) {
-    try {
-      console.log("Posting damage with data:", damageData);
-  
-      damageData.damageLevel = parseInt(damageData.damageLevel);
-      damageData.countAffected = parseInt(damageData.countAffected);
-       // const response = await fetch('https://group-project-25-1-the-anti-statics-api.onrender.com/api/v1/damages');
-      const response = await fetch('http://localhost:3000/api/v1/damages', {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(damageData),
-      });
-  
-      if (!response.ok) {
-        return { postError: "Failed to post damage. Please try again later." };
-      }
-  
-      const info = await response.json();
-      return { info }; // Return the response data
-    } catch (error) {
-      //Console the error
-      console.log(error);
-      //Return the error if the API is offline
-      return {
-        error: "The server is currently unreachable. Please try again later.",
-      };
+  try {
+    console.log("Posting damage with data:", damageData);
+
+    damageData.damageLevel = parseInt(damageData.damageLevel);
+    damageData.countAffected = parseInt(damageData.countAffected);
+    const response = await fetch('https://group-project-25-1-the-anti-statics-api.onrender.com/api/v1/damages', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(damageData),
+    });
+
+    if (!response.ok) {
+      return { postError: "Failed to post damage. Please try again later." };
     }
+
+    const info = await response.json();
+    return { info }; // Return the response data
+  } catch (error) {
+    //Console the error
+    console.log(error);
+    //Return the error if the API is offline
+    return {
+      error: "The server is currently unreachable. Please try again later.",
+    };
   }
-  
+}
