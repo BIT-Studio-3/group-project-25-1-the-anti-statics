4ce00f4eadea6ad56294f9de6d06f5c05ca88c13
Paras Paras <para12@student.op.ac.nz>
2025-04-09 14:04:04 +1200

    got the variables

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -4,4 +4,10 @@
   import user from "../../stores/user.js";
   import { login } from "../../stores/login.js";
   import { goto } from "$app/navigation";
+
+  let emailAddress = "";
+  let password = "";
+  let postError = "";
+  let error = "";
+  let data = "";
 </script>
