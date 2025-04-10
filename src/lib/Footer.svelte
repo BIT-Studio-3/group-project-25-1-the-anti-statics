6c7645d685dc191016486a52f43195fd6ff003f9
Adamgm3 <adamgm3@student.op.ac.nz>
2025-04-11 11:49:46 +1200

    chore: Slight change to the times on the footer, the footer also has trouble staying at the bottom of the screen on some pages

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -2,7 +2,7 @@
     <p>Copyright © 2024 Otago Polytechnic</p>
     <p>New Zealand 0800 762 786
         International +64 3 477 3014
-        Hours: Monday – Friday, 8.00am – 5.00pm
+        Hours: Monday – Friday, 8:00am – 5:00pm
         Email: info@op.ac.nz
     </p>
     <nav>
