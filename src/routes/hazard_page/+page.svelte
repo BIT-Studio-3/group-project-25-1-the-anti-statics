39584e22542eb0281c8bdae334cdefff7f168c81
Hazel <mapuhn1@student.op.ac.nz>
2025-04-17 22:42:56 +1200

    refactor : remove numbers as string format

--- src/routes/hazard_page/+page.svelte
+++ src/routes/hazard_page/+page.svelte
@@ -99,11 +99,11 @@
         <div class="form-group">
           <label for="level">Hazard Level</label>
           <select id="level" bind:value={level} required>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
+            <option value=1>1</option>
+            <option value=2>2</option>
+            <option value=3>3</option>
+            <option value=4>4</option>
+            <option value=5>5</option>
           </select>
         </div>
 
