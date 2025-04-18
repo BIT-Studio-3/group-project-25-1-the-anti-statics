4f6fff7601d958402e42af17dc8d2905a1a0ec2e
SamBat2005 <batcsg1@student.op.ac.nz>
2025-04-18 13:41:33 +1200

    feat: created global url file

---
+++ url/url.js
@@ -0,0 +1,5 @@
+const url = 'https://group-project-25-1-the-anti-statics-api.onrender.com'
+// Production URL: https://group-project-25-1-the-anti-statics-api.onrender.com
+// Staging URL: https://group-project-25-1-the-anti-statics-api-4tz3.onrender.com
+
+export default url
