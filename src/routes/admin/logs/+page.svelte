d8d5bb6da7192cb41e7d417b5b4b46d97a131a4c
SamBat2005 <batcsg1@student.op.ac.nz>
2025-05-07 16:33:57 +1200

    feat: added website logs page

---
+++ src/routes/admin/logs/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Admin from "../../../lib/Admin.svelte";
+  </script>
+  
+  <main>
+    <Admin />
+    <h1>Website Logs</h1>
+  </main>
+  
+  <style>
+    main {
+      display: grid;
+      grid-template-columns: 1fr 2.3fr;
+    }
+  </style>
+  
