5a5a968a6746507612edb55fc013668ee3535b28
SamBat2005 <batcsg1@student.op.ac.nz>
2025-05-07 16:32:02 +1200

    feat: added user management admin page

---
+++ src/routes/admin/users/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Admin from "../../../lib/Admin.svelte";
+</script>
+
+<main>
+  <Admin />
+  <h1>User Management</h1>
+</main>
+
+<style>
+  main {
+    display: grid;
+    grid-template-columns: 1fr 2.3fr;
+  }
+</style>
