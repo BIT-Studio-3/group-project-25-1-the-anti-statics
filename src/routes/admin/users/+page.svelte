aa0913ab469926294462e81aa6036a840d01aee8
SamBat2005 <batcsg1@student.op.ac.nz>
2025-05-07 19:55:30 +1200

    feat: added user filtering option

--- src/routes/admin/users/+page.svelte
+++ src/routes/admin/users/+page.svelte
@@ -13,37 +13,57 @@
     </section>
 
     <section id="user-background">
-        <section id="create-users">
-            <input type="text" placeholder="Search users..." />
-            <section id="filter-box">
-              <p>Filter by:</p>
-              <select name=""></select>
-              <select name=""></select>
-            </section>
-          </section>
+      <section id="create-users">
+        <input type="text" placeholder="Search users..." />
+        <section id="filter-box">
+          <p>Filter by:</p>
+          <select>
+            <option>All Roles</option>
+            <option>Administrator</option>
+            <option>Incident Controller</option>
+            <option>Planning Officer</option>
+            <option>Operations Officer</option>
+            <option>Logistics Officer</option>
+            <option>Viewer</option>
+          </select>
+          <select>
+            <option>All Status</option>
+            <option>Active</option>
+            <option>Inactive</option>
+            <option>Pending</option>
+          </select>
+        </section>
+      </section>
     </section>
   </section>
 </main>
 
 <style>
-  #user-background{
+  select {
+    padding: 1em;
+    border: 2px green solid;
+    border-radius: 7px;
+  }
+  #user-background {
     background-color: white;
     border-radius: 7px;
     box-shadow: -1px 1px 8px #333;
     margin-inline: 0.7em;
   }
-  #create-users input{
-    padding-left: 1em;
+  #create-users input {
+    padding-left: 0.9em;
+    border-radius: 7px;
+    border: 2px green solid;
   }
   #filter-box {
     display: flex;
-    background-color: yellow;
     margin-left: auto;
+    align-items: center;
+    gap: 10px;
   }
   #create-users {
-    background-color: red;
-    
     display: flex;
+    height: 3em;
   }
   h1 {
     background-color: white;
