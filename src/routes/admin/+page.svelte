b8464622874b6dd92525dd0b2a0d16fccc3fdb00
SamBat2005 <batcsg1@student.op.ac.nz>
2025-05-07 15:40:33 +1200

    feat: added basic styling for admin portal

---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+
+</script>
+
+<main>
+    <section>
+        <h1>Admin User</h1>
+    </section>
+    <section>
+        <h1>Portal</h1>
+    </section>
+</main>
+
+<style>
+    main{
+        display: grid;
+        grid-template-columns: 1fr 3fr;
+    }
+    section:nth-child(1){
+        background-color: rgb(1, 100, 1);
+        border-right: 3px solid white;
+    }
+    section:nth-child(1) *{
+        color: white;
+    }
+</style>
