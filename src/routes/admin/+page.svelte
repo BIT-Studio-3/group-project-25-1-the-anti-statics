83284839a7e7f5837ae59ac139eb7d0b5d82b2cd
SamBat2005 <batcsg1@student.op.ac.nz>
2025-05-08 10:19:13 +1200

    feat: added white header for admin dashboard page

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,23 +1,28 @@
 <script>
-    import Admin from "../../lib/Admin.svelte";
+  import Admin from "../../lib/Admin.svelte";
 </script>
 
 <main>
-    <Admin/>
-    <section>
-        <h1>Dashboard</h1>
-        <p>Active Users</p>
-        <p>Total Disasters</p>
-        <p>Active Disasters</p>
-        <p>Total Hazards</p>
-        <p>Total Damages</p>
-        <p>Filter hazards and damages by incident</p>
-    </section>
+  <Admin />
+  <section>
+    <h1>Dashboard</h1>
+    <p>Active Users</p>
+    <p>Total Disasters</p>
+    <p>Active Disasters</p>
+    <p>Total Hazards</p>
+    <p>Total Damages</p>
+    <p>Filter hazards and damages by incident</p>
+  </section>
 </main>
 
 <style>
-    main{
-        display: grid;
-        grid-template-columns: 1fr 2.3fr;
-    }
+  h1 {
+    background-color: white;
+    padding: 1em;
+    border-bottom: 1px solid rgb(182, 164, 164);
+  }
+  main {
+    display: grid;
+    grid-template-columns: 1fr 2.3fr;
+  }
 </style>
