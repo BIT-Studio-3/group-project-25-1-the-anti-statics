5ea3f63a78475dc218cab8315e76a76324c3e3a2
SamBat2005 <batcsg1@student.op.ac.nz>
2025-05-07 19:48:51 +1200

    feat: added user filtering section

--- src/routes/admin/users/+page.svelte
+++ src/routes/admin/users/+page.svelte
@@ -11,10 +11,40 @@
       <h3>Manage all system users and their permissions</h3>
       <button>+ Add New User</button>
     </section>
+
+    <section id="user-background">
+        <section id="create-users">
+            <input type="text" placeholder="Search users..." />
+            <section id="filter-box">
+              <p>Filter by:</p>
+              <select name=""></select>
+              <select name=""></select>
+            </section>
+          </section>
+    </section>
   </section>
 </main>
 
 <style>
+  #user-background{
+    background-color: white;
+    border-radius: 7px;
+    box-shadow: -1px 1px 8px #333;
+    margin-inline: 0.7em;
+  }
+  #create-users input{
+    padding-left: 1em;
+  }
+  #filter-box {
+    display: flex;
+    background-color: yellow;
+    margin-left: auto;
+  }
+  #create-users {
+    background-color: red;
+    
+    display: flex;
+  }
   h1 {
     background-color: white;
     padding: 1em;
@@ -27,10 +57,10 @@
   #container {
     background-color: rgba(255, 255, 255, 0.5);
   }
-  #container section{
+  #container section {
     padding: 1em;
   }
-  button{
+  button {
     padding: 0.5em;
     margin-top: 1em;
     background-color: green;
