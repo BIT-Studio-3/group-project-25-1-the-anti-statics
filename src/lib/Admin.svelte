b7482de696acdc615e5e7174a8e0deccac3ca56d
SamBat2005 <batcsg1@student.op.ac.nz>
2025-05-07 15:57:47 +1200

    feat: added profile section in admin component

--- src/lib/Admin.svelte
+++ src/lib/Admin.svelte
@@ -1,19 +1,46 @@
 <script>
-
+  import pfp from "/src/lib/Images/pfp.png";
 </script>
 
 <main>
-    <section>
-        <h1>Admin User</h1>
+  <section>
+    <h1>🛡️ Admin Portal</h1>
+    <section id="user">
+      <img src={pfp} alt="pfp" height="80" />
+      <section id="profile">
+        <p>Samuel Batchelor</p>
+        <p>Administrator</p>
+      </section>
     </section>
+  </section>
 </main>
 
 <style>
-    section{
-        background-color: rgb(1, 100, 1);
-        border-right: 3px solid white;
-    }
-    section *{
-        color: white;
-    }
+  main {
+    background-color: rgb(1, 100, 1);
+    border-right: 3px solid white;
+  }
+  section * {
+    color: white;
+  }
+  h1 {
+    text-align: center;
+    padding: 1em;
+  }
+  #user {
+    border-top: 2px solid rgb(0, 75, 13);
+    border-bottom: 2px solid grey;
+    padding: 0.5em;
+    display: flex;
+    align-items: center;
+  }
+  #user p:nth-child(1) {
+    font-size: larger;
+  }
+  #user p:nth-child(2) {
+    font-size: small;
+  }
+  #profile{
+    margin-left: 0.8em;
+  }
 </style>
