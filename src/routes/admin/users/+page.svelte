91cb7681ec4587ff76ebef9b94d77eb601ec9fa1
SamBat2005 <batcsg1@student.op.ac.nz>
2025-05-07 19:28:47 +1200

    feat: added create user button

--- src/routes/admin/users/+page.svelte
+++ src/routes/admin/users/+page.svelte
@@ -4,12 +4,39 @@
 
 <main>
   <Admin />
-  <h1>User Management</h1>
+  <section id="container">
+    <h1>User Management</h1>
+    <section>
+      <h2>User Accounts</h2>
+      <h3>Manage all system users and their permissions</h3>
+      <button>+ Add New User</button>
+    </section>
+  </section>
 </main>
 
 <style>
+  h1 {
+    background-color: white;
+    padding: 1em;
+  }
   main {
     display: grid;
     grid-template-columns: 1fr 2.3fr;
+    overflow-y: auto;
+  }
+  #container {
+    background-color: rgba(255, 255, 255, 0.5);
+  }
+  #container section{
+    padding: 1em;
+  }
+  button{
+    padding: 0.5em;
+    margin-top: 1em;
+    background-color: green;
+    border: 3px solid green;
+    border-radius: 7px;
+    color: white;
+    font-size: medium;
   }
 </style>
