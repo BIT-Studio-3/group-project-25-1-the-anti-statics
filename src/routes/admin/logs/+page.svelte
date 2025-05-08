b9956075b4ea6a4b17a366d283280e5cb718a5a4
SamBat2005 <batcsg1@student.op.ac.nz>
2025-05-08 21:32:00 +1200

    feat: added scroll icon to website logs header

--- src/routes/admin/logs/+page.svelte
+++ src/routes/admin/logs/+page.svelte
@@ -1,11 +1,15 @@
 <script>
   import Admin from "../../../lib/Admin.svelte";
+  import Log from "../../../lib/SVGs/scroll-solid.svg";
 </script>
 
 <main>
   <Admin />
   <section id="container">
-    <h1>Website Logs</h1>
+    <header>
+      <img src={Log} alt="log" height=20 />
+      <h1>Website Logs</h1>
+    </header>
   </section>
 </main>
 
@@ -19,9 +23,12 @@
     overflow-x: hidden;
     height: 85vh;
   }
-  h1 {
+  header {
     background-color: white;
     padding: 1em;
     border-bottom: 1px solid rgb(182, 164, 164);
+    display: flex;
+    gap: 1em;
+    align-items: center;
   }
 </style>
