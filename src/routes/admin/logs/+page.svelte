3693337dbdbd535e75f6705d2572c78c2a72842c
SamBat2005 <batcsg1@student.op.ac.nz>
2025-05-08 20:53:55 +1200

    feat: added container for website logs page

--- src/routes/admin/logs/+page.svelte
+++ src/routes/admin/logs/+page.svelte
@@ -1,16 +1,22 @@
 <script>
-    import Admin from "../../../lib/Admin.svelte";
-  </script>
-  
-  <main>
-    <Admin />
+  import Admin from "../../../lib/Admin.svelte";
+</script>
+
+<main>
+  <Admin />
+  <section id="container">
     <h1>Website Logs</h1>
-  </main>
-  
-  <style>
-    main {
-      display: grid;
-      grid-template-columns: 1fr 2.3fr;
-    }
-  </style>
-  
+  </section>
+</main>
+
+<style>
+  main {
+    display: grid;
+    grid-template-columns: 1fr 2.3fr;
+  }
+  #container {
+    background-color: rgba(255, 255, 255, 0.5);
+    overflow-x: hidden;
+    height: 85vh;
+  }
+</style>
