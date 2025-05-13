60cf3355cd758ec454b6a790962a011ec93276ed
Hazel <mapuhn1@student.op.ac.nz>
2025-05-13 12:36:31 +1200

    feat: create disaster object

--- src/routes/incident_controller/+page.svelte
+++ src/routes/incident_controller/+page.svelte
@@ -1,34 +1,46 @@
 <script>
-  import IncidentControl from "../../lib/IncidentControl.svelte";
-  
+    import IncidentControl from "../../lib/IncidentControl.svelte";
+
+    let disaster = {
+        title: "",
+        type: "",
+        location: "",
+        description: "",
+        status: "",
+        severity: "",
+        controllerId: "",
+        startTime: "",
+        endTime: "",
+    };
 </script>
 
 <main>
-  <IncidentControl />
-  <section id="container">
-    <header>
-      <h1>Create New Incident</h1><br>
-      <p>Report and track issues efficiently</p>
-    </header>
-  </section>
+    <IncidentControl />
+    <section id="container">
+        <header>
+            <h1>Create New Incident</h1>
+            <br />
+            <p>Report and track issues efficiently</p>
+        </header>
+    </section>
 </main>
 
 <style>
-  main {
-    display: grid;
-    grid-template-columns: 1fr 2.3fr;
-  }
-  #container {
-    background-color: rgba(255, 255, 255, 0.5);
-    overflow-x: hidden;
-    height: 85vh;
-  }
-  header {
-    background-color: white;
-    padding: 1em;
-    border-bottom: 1px solid rgb(182, 164, 164);
-    display: flex;
-    gap: 1em;
-    align-items: center;
-  }
+    main {
+        display: grid;
+        grid-template-columns: 1fr 2.3fr;
+    }
+    #container {
+        background-color: rgba(255, 255, 255, 0.5);
+        overflow-x: hidden;
+        height: 85vh;
+    }
+    header {
+        background-color: white;
+        padding: 1em;
+        border-bottom: 1px solid rgb(182, 164, 164);
+        display: flex;
+        gap: 1em;
+        align-items: center;
+    }
 </style>
