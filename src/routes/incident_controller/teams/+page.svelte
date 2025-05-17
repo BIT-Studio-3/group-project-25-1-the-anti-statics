b44d750631edf53cf713ee0c34860fd3f9bf6bfc
Hazel <mapuhn1@student.op.ac.nz>
2025-05-17 22:13:58 +1200

    style: format file

--- src/routes/incident_controller/teams/+page.svelte
+++ src/routes/incident_controller/teams/+page.svelte
@@ -38,7 +38,9 @@
     <section id="container">
         <header>
             <h1>New Response Team</h1>
-            <p class="subtitle">Create and track disaster response teams efficiently</p>
+            <p class="subtitle">
+                Create and track disaster response teams efficiently
+            </p>
         </header>
 
         <div class="form-container">
