e46ba59f09673ec4763d81228aed064078758577
SamBat2005 <batcsg1@student.op.ac.nz>
2025-05-27 18:16:39 +1200

    feat: added disaster fetching function in teams page

---
+++ src/routes/incident_controller/teams/+page.js
@@ -0,0 +1,26 @@
+import url from "/src/url/url";
+export async function load() {
+    try {
+        //Fetch the data from the back-end API
+        const response = await fetch(`${url}/api/v1/disasters`);
+        
+        //If the response has no data
+        if(!response.ok){
+            return {message: "No disasters available"};
+        }
+        
+        //Fetch the data in json format
+        const disasters = await response.json();
+        
+        //Console the data
+        console.log(disasters);
+        
+        //Return the data
+        return {disasters: disasters.data};
+    } catch (error) {
+        //Console the error
+        console.log('Error fetching data:', error);
+        //Return the error if the API is offline
+        return {error: 'The server is currently unreachable. Please try again later.'}
+    }
+}
