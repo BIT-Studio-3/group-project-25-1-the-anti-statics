0b38fabab9ce4bdbf139267c0c3fa7194e166e55
SamBat2005 <batcsg1@student.op.ac.nz>
2025-05-28 21:02:16 +1200

    refactor: changed global API endpoint URL to production url

--- src/url/url.js
+++ src/url/url.js
@@ -1,4 +1,4 @@
-const url = 'http://localhost:3000'
+const url = 'https://group-project-25-1-the-anti-statics-api.onrender.com'
 // Production URL: https://group-project-25-1-the-anti-statics-api.onrender.com
 // Staging URL: https://group-project-25-1-the-anti-statics-api-4tz3.onrender.com
 // Development: http://localhost:3000
