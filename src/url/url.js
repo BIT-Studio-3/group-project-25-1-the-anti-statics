3bec106634f65883711565b2bba6eec845b5a888
Samuel Batchelor <batcsg1@student.op.ac.nz>
2025-05-30 12:20:50 +1200

    refactor: changed global url to staging API url

--- src/url/url.js
+++ src/url/url.js
@@ -1,4 +1,4 @@
-const url = 'https://group-project-25-1-the-anti-statics-api.onrender.com'
+const url = 'https://group-project-25-1-the-anti-statics-api-4tz3.onrender.com'
 // Production URL: https://group-project-25-1-the-anti-statics-api.onrender.com
 // Staging URL: https://group-project-25-1-the-anti-statics-api-4tz3.onrender.com
 // Development: http://localhost:3000
