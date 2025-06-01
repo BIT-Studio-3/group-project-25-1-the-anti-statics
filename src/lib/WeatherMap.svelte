e53fca6408b5c74fab053af2953a34e8e93bc074
Hazel <mapuhn1@student.op.ac.nz>
2025-06-02 04:58:18 +1200

    feat: create marker manager

--- src/lib/WeatherMap.svelte
+++ src/lib/WeatherMap.svelte
@@ -24,3 +24,16 @@
     // Waiting that the map is "loaded"
     await map.onReadyAsync();
 
+    // The marker manager is in charge of computing the positions where markers
+    // should be, sort them by POI rank and select non-overlaping places.
+    const markerManager = new maptilermarkerlayout.MarkerLayout(map, {
+      layers: ["City labels", "Place labels"],
+      markerSize: [40, 70],
+      offset: [0, -10],
+      markerAnchor: "center",
+      filter: (feature) => {
+        return ["city", "village", "town"].includes(
+          feature.properties.class
+        );
+      },
+    });
