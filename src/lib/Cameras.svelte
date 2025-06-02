2049890e14545b858ffe8ef78bb0fa648a066882
SamBat2005 <batcsg1@student.op.ac.nz>
2025-06-03 11:01:03 +1200

    feat: added camera component

---
+++ src/lib/Cameras.svelte
@@ -0,0 +1,32 @@
+<script>
+  export let data;
+  const { cameras } = data;
+  console.log(cameras);
+  let selectedCameraId = null;
+  let selectedCamera = null;
+  // Find the selected camera based on the ID
+  $: selectedCamera = cameras.find(
+    (camera) => camera.id === selectedCameraId
+  );
+</script>
+
+<section>
+  <h2 class="text-2xl font-bold mb-2">NZTA Traffic Camera</h2>
+
+  <select bind:value={selectedCameraId}>
+    <option value="" disabled>Select a camera</option>
+    {#each cameras as camera}
+      <option value={camera.id}>{camera.name}</option>
+    {/each}
+  </select>
+
+  {#if selectedCamera}
+    <div>
+      <img
+        src={selectedCamera.imageUrl}
+        alt={selectedCamera.name || "Image Loading"}
+      />
+      <p>{selectedCamera.description}</p>
+    </div>
+  {/if}
+</section>
