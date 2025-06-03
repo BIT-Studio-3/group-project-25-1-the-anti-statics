dd7c361e802a2bdc37869f5a61a228ecd19858be
SamBat2005 <batcsg1@student.op.ac.nz>
2025-06-03 15:32:12 +1200

    feat: added styling to select a camera box

--- src/lib/Cameras.svelte
+++ src/lib/Cameras.svelte
@@ -9,36 +9,45 @@
 </script>
 
 <main>
-    <h1>🛣️ Road Conditions Page</h1>
+  <h1>🛣️ Road Conditions Page</h1>
 
+  <section id="camera-select">
+    <h2>Select a camera:</h2>
     <select bind:value={selectedCameraId}>
-      <option value="" disabled>Select a camera</option>
       {#each cameras as camera}
         <option value={camera.id}>{camera.name}</option>
       {/each}
     </select>
+  </section>
 
-    <div>
-      {#if selectedCamera}
-        <img
-          src={selectedCamera.imageUrl}
-          alt={selectedCamera.name || "Image Loading"}
-        />
-        <p>{selectedCamera.description}</p>
-      {:else}
-        <img 
-        src={cameras[0].imageUrl}
-        alt={cameras[0].name || "Image Loading"}
-        />
-        <p>{cameras[0].description}</p>
-        />
-      {/if}
-    </div>
+  <div>
+    {#if selectedCamera}
+      <img
+        src={selectedCamera.imageUrl}
+        alt={selectedCamera.name || "Image Loading"}
+      />
+      <p>{selectedCamera.description}</p>
+    {:else}
+      <img src={cameras[0].imageUrl} alt={cameras[0].name || "Image Loading"} />
+      <p>{cameras[0].description}</p>
+      />
+    {/if}
+  </div>
 </main>
 
 <style>
+  #camera-select{
+    background-color: white;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 1em 3em 1em 3em;
+  }
+  #camera-select h2{
+    color: #333;
+  }
   select {
-    padding: 1em;
+    padding: 1em 4em 1em 4em;
   }
   main {
     background-color: rgba(1, 100, 1, 0.4);
@@ -49,5 +58,8 @@
   h1 {
     color: white;
     padding: 1em;
+  }
+  h2 {
+    color: white;
   }
 </style>
