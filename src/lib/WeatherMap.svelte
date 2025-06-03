6113dbf8b218ee8ddc90ec84b6f245967776d8fe
Hazel <mapuhn1@student.op.ac.nz>
2025-06-03 19:24:34 +1200

    fix: add z-index to fix marker translation

--- src/lib/WeatherMap.svelte
+++ src/lib/WeatherMap.svelte
@@ -257,14 +257,16 @@
   z-index: 1;
 }
 
-  .marker-container {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    pointer-events: none;
-  }
+.marker-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  pointer-events: none;
+  z-index: 2;
+  overflow: hidden;
+}
 
   :global(.marker) {
     position: absolute;
