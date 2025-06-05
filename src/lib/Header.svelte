c8c4ffffa73cefbb0d7eacf122c551afb5be327e
Hazel <mapuhn1@student.op.ac.nz>
2025-06-06 07:06:42 +1200

    feat: on incident controller page click use close menu function

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -64,7 +64,7 @@
         <li in:fly={{ y: 30, duration: 500 }}><a href="/admin" on:click={closeMenu}>Admin</a></li>
         <li in:fly={{ y: 30, duration: 600 }}><a href="/damages" on:click={closeMenu}>Damages</a></li>
         <li in:fly={{ y: 30, duration: 800 }}><a href="/hazards" on:click={closeMenu}>Hazards</a></li>
-        <li in:fly={{ y: 30, duration: 900 }}><a href="/incident_controller">Incident Controller</a></li>
+        <li in:fly={{ y: 30, duration: 900 }}><a href="/incident_controller" on:click={closeMenu}>Incident Controller</a></li>
         <li><button class="theme-toggle" on:click={toggleTheme}>
           {#if $isDark}☀️ Light{:else}🌙 Dark{/if}
         </button></li>
